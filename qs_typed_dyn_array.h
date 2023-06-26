42f8fe9a0643667e3448ea43f0f47a0094193b60
duffhd <duffhd@protonmail.com>
2023-06-25 21:28:12 -0300

    Fixed name collision with dyn_array that uses void*, change the header name to include 'typed'

--- qs_typed_dyn_array.h
+++ qs_typed_dyn_array.h
@@ -6,15 +6,21 @@
 #include <memory.h>
 #include <stdlib.h>
 
+/**
+ * A macro defined array that receives the type of the data inside it, the array struct will be name as follows:
+ * QS_DYN_ARRAY_DEFINE(int)
+ * The generated array will have the following name:
+ * QsDynArray_int
+ */
 #define QS_DYN_ARRAY_DEFINE(type) \
                                   \
 typedef struct {                  \
     type   *data;                 \
-    size_t size;                  \
+    size_t len;                   \
     size_t capacity;              \
 } QsDynArray_##type;              \
                                   \
-QsDynArray_##type *qs_dyn_array_alloc(size_t size) \
+QsDynArray_##type *qs_dyn_array_##type##_alloc(size_t size) \
 { \
     QsDynArray_##type *array = malloc(sizeof(QsDynArray_##type)); \
     if (array == NULL) {                                          \
@@ -25,27 +31,27 @@ QsDynArray_##type *qs_dyn_array_alloc(size_t size) \
         free(array);                                              \
         return NULL;                                              \
     }                                                             \
-    array->size       = 0;                                        \
+    array->len      = 0;                                          \
     array->capacity = size;                                       \
     return array;                                                 \
 } \
 \
-int qs_dyn_array_append(QsDynArray_##type *array, const type *item) \
+size_t qs_dyn_array_##type##_append(QsDynArray_##type *array, const type *item) \
 { \
-    if(array->size == array->capacity) {         \
-        array->capacity += array->capacity / 2;  \
-        type *new_data = realloc(array->data, )  \
-        if (!new_data) return -1;                \
-        array->data = new_data;                  \
-    }                                            \
-                                                 \
-    array->data[array->size] = &item;            \
-    array->size++;                               \
-    return array->size;                          \
+    if(array->len == array->capacity) {                         \
+        array->capacity += array->capacity / 2;                 \
+        type *new_data = realloc(array->data, array->capacity); \
+        if (!new_data) return -1;                               \
+        array->data = new_data;                                 \
+    }                                                           \
+                                                                \
+    array->data[array->len] = *item;                            \
+    array->len++;                                               \
+    return array->len;                                          \
 } \
 \
-type *qs_dyn_array_get(QsDynArray_##type *array, size_t index) \
+type *qs_dyn_array_##type##_get(QsDynArray_##type *array, size_t index) \
 { \
-    if (index > array->size) return NULL; \
+    if (index > array->len) return NULL;  \
     return &array->data[index];           \
 }
