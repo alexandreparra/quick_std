0afb266230213e595ffb6dec9f75a1dc9d3ed4e9
Alexandre Parra <duffhd@outlook.com>
2024-03-12 19:46:57 -0300

    Single include header for all data structures

---
+++ qs.h
@@ -0,0 +1,9 @@
+#pragma once 
+
+// Group all structures and #define under the same header file.
+
+#include "qs_def.h"
+#include "qs_arena.h"
+#include "qs_array.h"
+#include "qs_string.h"
+#include "qs_ring_buffer.h"
